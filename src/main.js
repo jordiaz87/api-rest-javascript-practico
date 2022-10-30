3410b4fdee5471a0c90e09128c749220820c7368
Jorge Diaz <jorjorf@gmail.com>
2022-10-30 14:55:02 -0400

    Infinite Scrolling: limitando la carga de datos

--- src/main.js
+++ src/main.js
@@ -134,12 +134,9 @@ async function getMoviesBySearch(query) {
 async function getTrendingMovies() {
     const { data } = await api('trending/movie/day');
     const movies = data.results;
+    maxPage = data.total_pages;
     createMovies(movies, genericSection, { lazyLoad: true, clean: true});
 
-    // const btnLoadMore = document.createElement('button');
-    // btnLoadMore.innerText = 'Cargar mas';
-    // btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
-    // genericSection.appendChild(btnLoadMore);
 }
 
 
@@ -155,7 +152,10 @@ async function getPaginatedTrendingMovies() {
 
     const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight -15);
 
-    if (scrollIsBottom){
+    const pageIsNotMax = page < maxPage;
+
+
+    if (scrollIsBottom && pageIsNotMax){
         page++;
         const { data } = await api('trending/movie/day', {
         params: {
@@ -165,10 +165,7 @@ async function getPaginatedTrendingMovies() {
         const movies = data.results;
         createMovies(movies, genericSection, { lazyLoad: true, clean: false});
 
-        // const btnLoadMore = document.createElement('button');
-        // btnLoadMore.innerText = 'Cargar mas';
-        // btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
-        // genericSection.appendChild(btnLoadMore);
+    
     }
 
 
