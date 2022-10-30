c96705fe8e570ed25cc228dd6b22e7e2ec393a1d
Jorge Diaz <jorjorf@gmail.com>
2022-10-30 16:29:38 -0400

    Botón de like

--- src/main.js
+++ src/main.js
@@ -39,9 +39,7 @@ function createMovies (
     movies.forEach(movie => {
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
-        movieContainer.addEventListener('click', () => {
-            location.hash = '#movie=' + movie.id;
-        })
+        
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
@@ -50,18 +48,32 @@ function createMovies (
             'https://image.tmdb.org/t/p/w300' + movie.poster_path,
         );
 
+        movieImg.addEventListener('click', () => {
+            location.hash = '#movie=' + movie.id;
+        })
+
         movieImg.addEventListener('error', () => {
             movieImg.setAttribute(
                 'src',
                 'https://static.platzi.com/static/images/error/img404.png',
         );
         });
+
+        const movieBtn = document.createElement('button');
+        movieBtn.classList.add('movie-btn');
+        movieBtn.addEventListener('click', () => {
+            movieBtn.classList.toggle('movie-btn--liked');
+            // deberiamos agregar la pelicula a localStorage
+        });
+
+
         if (lazyLoad) {
             lazyLoader.observe(movieImg);
         }
         
 
         movieContainer.appendChild(movieImg);
+        movieContainer.appendChild(movieBtn);
         container.appendChild(movieContainer);
     });
 }
